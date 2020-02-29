28e1dec3b06675bef715c0049f35e214d2d79c6d
bridgelancer <thchoy@hotmail.com>
2020-02-29 19:58:23 +0800

    Default to prefixed served route
    Note:
    -   This is to avoid build error while deploying

--- src/utils/sockets/openSocket.tsx
+++ src/utils/sockets/openSocket.tsx
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.APP_HOST_DOMAIN, {transports: ['websocket']})
+const socket = io(process.env.APP_HOST_DOMAIN || 'https://conway-game-of-life-backend.herokuapp.com', {transports: ['websocket']})
 export default socket;
