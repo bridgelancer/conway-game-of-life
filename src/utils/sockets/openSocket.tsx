8d7b4fce98f991792ae9417a5acce8f1c40897f1
bridgelancer <thchoy@hotmail.com>
2020-02-29 22:15:16 +0800

    Update key to REACT_APP_HOST_DOMAIN

--- src/utils/sockets/openSocket.tsx
+++ src/utils/sockets/openSocket.tsx
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.APP_HOST_DOMAIN || 'https://conway-game-of-life-backend.herokuapp.com', {transports: ['websocket']})
+const socket = io(process.env.REACT_APP_HOST_DOMAIN || 'https://conway-game-of-life-backend.herokuapp.com/test', {transports: ['websocket']})
 export default socket;
