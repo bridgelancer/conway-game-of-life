f278038bfd994fbc7cff24e52c52b6257c9b6590
bridgelancer <thchoy@hotmail.com>
2020-03-01 23:08:31 +0800

    Use block.svg as header icon
    Note:
    -   Remove test route also

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Express server that servers the static files built from build directory
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
@@ -5,13 +6,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/build/block.svg'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
